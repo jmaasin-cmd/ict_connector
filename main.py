639bfb7602ce24526bc2dd38c2259af572588f61
jmaasin-cmd <jmaasin@southernleytestateu.edu.ph>
2026-05-04 15:11:07 +0800

    Update Laravel API endpoint in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import httpx
 app = FastAPI()
 
 # CONFIG
-LARAVEL_API = "http://46.250.235.51/api"
+LARAVEL_API = "https://ict.ccsit.info/api"
 PREDICT_API = "https://copra-fastapi3.onrender.com/predict"
 
 # STATE
